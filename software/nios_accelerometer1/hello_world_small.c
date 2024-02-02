f282c123446c565b8c691bf260039abe13651792
Gurjan-Singh-Samra <gsamra474@gmail.com>
2024-02-02 10:59:43 +0000

    added changes to lab3

--- software/nios_accelerometer1/hello_world_small.c
+++ software/nios_accelerometer1/hello_world_small.c
@@ -8,6 +8,8 @@
 
 #define OFFSET -32
 #define PWM_PERIOD 16
+#define FILTER_TAP_NUM 4
+//#define COEFFICIENT 0.5f
 
 alt_8 pwm = 0;
 alt_u8 led;
@@ -17,10 +19,30 @@ void led_write(alt_u8 led_pattern) {
     IOWR(LED_BASE, 0, led_pattern);
 }
 
+float applyFilter(float new_reading){
+      static float filter[FILTER_TAP_NUM] = {0};
+      static float coeff[] = {-0.1204, 0.2879, 0.6369, 0.2879};
+      float filtered_reading = 0;
+
+      for(int i = FILTER_TAP_NUM - 1; i > 0; i--){
+            filter[i] = filter[i-1];
+      }
+
+      filter[0] = new_reading;
+
+      for(int i = 0; i < FILTER_TAP_NUM; i++){
+            filtered_reading += filter[i] * coeff[i];
+      }
+      return filtered_reading;
+}
+
+
 void convert_read(alt_32 acc_read, int * level, alt_u8 * led) {
     acc_read += OFFSET;
     alt_u8 val = (acc_read >> 6) & 0x07;
+    alt_printf("val: %x\n", val);
     * led = (8 >> val) | (8 << (8 - val));
+    alt_printf("led: %x\n", (8 >> val) | (8 << (8 - val)));
     * level = (acc_read >> 1) & 0x1f;
 }
 
@@ -71,8 +93,16 @@ int main() {
     while (1) {
 
         alt_up_accelerometer_spi_read_x_axis(acc_dev, & x_read);
-        // alt_printf("raw data: %x\n", x_read);
-        convert_read(x_read, & level, & led);
+
+//        float x = (float)x_read;
+
+        alt_32 filtered_reading = (alt_32)applyFilter((float)x_read);
+
+//        int level;
+//        alt_u8 led;
+        alt_printf("raw data: %x\n", x_read);
+        alt_printf("filtered data: %x\n", filtered_reading);
+        convert_read(filtered_reading, &level, &led);
 
     }
 
